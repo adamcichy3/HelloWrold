074ef62cd90769b1f2b271ab1ca9977b1d84dca9
gizd <adam.cichyy3@gmail.com>
2015-10-13 11:48:45 +0200

    Change-Id: Ie5b6a8bf7ad511c1babb0ba703e5ff164a8c4641 Signed-off-by: gizd <adam.cichyy3@gmail.com>

---
+++ src/main/java/com/ATH/Labolaboratorium/App.java
@@ -0,0 +1,13 @@
+package com.ATH.Labolaboratorium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
